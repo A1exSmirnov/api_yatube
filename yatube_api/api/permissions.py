efbb1ebf7cd53fce29557eca7198bd4ec9ddacfd
Александр Смирнов <cnccnc991@gmail.com>
2022-03-29 03:36:56 +0300

    api_final_yatube

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -13,8 +13,4 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        elif request.user.is_anonymous and (
-            request.method not in permissions.SAFE_METHODS
-        ):
-            return False
         return obj.author == request.user
